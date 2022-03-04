d2dcf926ce91962f595ed569b3658980013b2764
EvgeniBlag <blagodarni@gmail.com>
2022-03-04 16:36:18 +0100

    change

--- game.js
+++ game.js
@@ -1,13 +1,17 @@
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 
-
+var aster = [];
+var timer = 0;
 
 var asterimg = new Image();
 asterimg.src = "aster.png";
 
 var fonimg = new Image();
 fonimg.src = "fon.png";
+
+
+aster.push({x:0,y:300,dx:10,dy:20});
 
 
 
@@ -23,12 +27,29 @@ function game () {
 }
 
 function update() {
-
+timer++;
+if (timer%10==0) {
+    aster.push({
+        x:Math.random()*600,
+        y:-50,
+        dx:Math.random()*2-1,
+        dy:Math.random()*2+2
+    });
 }
+
+//Fisica    
+for(i in aster){ 
+aster[i].x=aster[i].x+aster[i].dx;
+aster[i].y=aster[i].y+aster[i].dy;
+//board
+ if (aster[i].x>=550 || aster[i].x<0) aster[i].dx=-aster[i].dx;
+ if (aster[i].y>=600 ) aster.slice;
+}
+ }
 
 function render (){
     context.drawImage(fonimg,0,0,600,600);
-    context.drawImage(asterimg,0,0,50,50);
+    for(i in aster) context.drawImage(asterimg,aster[i].x,aster[i].y,50,50);
 
 }
 
