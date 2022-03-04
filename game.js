6542c0e97cbab271d487b17dfd9fa13ac9817eee
EvgeniBlag <blagodarni@gmail.com>
2022-03-04 10:45:31 +0100

    Somethink

--- game.js
+++ game.js
@@ -1,2 +1,9 @@
-var canvas = document.getElementById("geme");
+var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
+
+var fonimg = new Image();
+fonimg.src = "fon.png";
+
+fonimg.onload = function () {
+  context.drawImage(fonimg,0,0,600,600);
+}
