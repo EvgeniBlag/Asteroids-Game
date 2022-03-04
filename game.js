3abb84f48a7895ff5810f3e050862b5383c9ac65
EvgeniBlag <blagodarni@gmail.com>
2022-03-04 11:08:28 +0100

    requestAnimation

--- game.js
+++ game.js
@@ -5,5 +5,30 @@ var fonimg = new Image();
 fonimg.src = "fon.png";
 
 fonimg.onload = function () {
-  context.drawImage(fonimg,0,0,600,600);
+ game();
 }
+
+function game () {
+    update();
+    render();
+    requestAnimationFrame(game);
+}
+
+function update() {
+
+}
+
+function render (){
+    context.drawImage(fonimg,0,0,600,600);
+
+}
+
+var requestAnimationFrame = (function(){
+    return window.requestAnimationFrame||
+
+    window.webkitRequestAnimationFrame||
+    window.mozRequestAnimationFrame   ||
+    function(callback){
+     window.setTimeout(callback,1000 / 20);
+    };
+})();
