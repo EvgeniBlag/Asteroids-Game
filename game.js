51daed4c5edbea636a52211a0115265b96e181a8
EvgeniBlag <blagodarni@gmail.com>
2022-03-04 12:09:53 +0100

    Asteroid

--- game.js
+++ game.js
@@ -1,12 +1,20 @@
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 
+
+
+var asterimg = new Image();
+asterimg.src = "aster.png";
+
 var fonimg = new Image();
 fonimg.src = "fon.png";
+
+
 
 fonimg.onload = function () {
  game();
 }
+//basic game cicle
 
 function game () {
     update();
@@ -20,8 +28,11 @@ function update() {
 
 function render (){
     context.drawImage(fonimg,0,0,600,600);
+    context.drawImage(asterimg,0,0,50,50);
 
 }
+
+
 
 var requestAnimationFrame = (function(){
     return window.requestAnimationFrame||
